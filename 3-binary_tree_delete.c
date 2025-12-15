be10f3adb0fa9f7ff34c474fe834acad00f29b63
JCeroOne <jceroone@gmail.com>
2025-12-15 22:34:05 +0000

    Patch

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a binary tree
+ * @tree: A pointer to the root of the binary tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	delete_nodes_recursively(tree);
+}
+
+/**
+ * delete_nodes_recursively - Recursively deletes a binary tree
+ * @node: The node to delete
+ */
+void delete_nodes_recursively(binary_tree_t *node)
+{
+	if (node == NULL)
+		return;
+
+	if (node->left != NULL)
+		delete_nodes_recursively(node->left);
+
+	if (node->right != NULL)
+		delete_nodes_recursively(node->right);
+
+	free(node);
+}
