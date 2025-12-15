ffc021143ad739e187a64a3c43b58ae010598e92
JCeroOne <jceroone@gmail.com>
2025-12-15 21:50:59 +0000

    Patch

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
 	if (node == NULL || parent == NULL)
+	{
+		if (node != NULL)
+			free(node);
+
 		return (NULL);
+	}
 
 	node->parent = parent;
 	node->n = value;
