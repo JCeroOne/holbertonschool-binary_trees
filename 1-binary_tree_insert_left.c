1e13154e7cc1fddbc706ed6e05ad4122b318abdc
JCeroOne <jceroone@gmail.com>
2025-12-15 16:57:56 +0000

    Patch

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Sets a node as the left child of the parent.
+ * @parent: A pointer to the parent node.
+ * @value: The value of the new node.
+ *
+ * Return: A pointer to the new node.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+	node->left = parent->left;
+	node->right = NULL;
+
+	parent->left = node;
+
+	return (node);
+}
+
