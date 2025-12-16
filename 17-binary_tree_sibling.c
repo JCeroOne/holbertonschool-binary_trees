3df7eaa5157ebe3b5c763d48eb35b692d8c9e4e9
JCeroOne <jceroone@gmail.com>
2025-12-16 03:27:40 +0000

    New task

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Gets the sibling of the specified node
+ * @node: The node
+ *
+ * Return: A pointer to the sibling of the specified node.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	int isLeft = 0;
+
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent == NULL)
+		return (NULL);
+
+	if (node->n < node->parent->n)
+		isLeft = 1;
+
+	return (isLeft == 1 ? node->parent->left : node->parent->right);
+}
